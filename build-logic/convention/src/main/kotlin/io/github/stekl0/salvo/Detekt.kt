cd0ba5e4c1b359bd73ae9258fd83def7969b37cd
stekl0 <greystekl0@gmail.com>
2025-09-06 22:49:41 +0300

    Enhance CI configuration and update Detekt/Ktlint settings for improved reporting

--- build-logic/convention/src/main/kotlin/io/github/stekl0/salvo/Detekt.kt
+++ build-logic/convention/src/main/kotlin/io/github/stekl0/salvo/Detekt.kt
@@ -1,17 +1,28 @@
 package io.github.stekl0.salvo
 
+import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.withType
 
 internal fun Project.configureDetekt() {
     extensions.configure<DetektExtension> {
-        config.setFrom(rootProject.files("config/detekt/detekt.yml"))
+        toolVersion = "1.23.8"
 
         buildUponDefaultConfig = true
         allRules = false
         parallel = true
 
+        config.setFrom(rootProject.files("config/detekt/detekt.yml"))
+
         dependencies.add("detektPlugins", libs.findLibrary("detekt.composeRules").get())
+    }
+
+    tasks.withType<Detekt>().configureEach {
+        reports {
+            sarif.required.set(true)
+            sarif.outputLocation.set(file("build/reports/detekt/$name.sarif"))
+        }
     }
 }
--- build-logic/convention/src/main/kotlin/io/github/stekl0/salvo/Ktlint.kt
+++ build-logic/convention/src/main/kotlin/io/github/stekl0/salvo/Ktlint.kt
@@ -3,13 +3,26 @@ package io.github.stekl0.salvo
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.jlleitschuh.gradle.ktlint.KtlintExtension
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 internal fun Project.configureKtlint() {
     extensions.configure<KtlintExtension> {
-        filter {
-            exclude("**/build/**")
+        version.set("1.7.1")
+
+        reporters {
+            reporter(ReporterType.CHECKSTYLE)
+            reporter(ReporterType.SARIF)
         }
 
+        filter {
+            exclude("**/build/**")
+            exclude("**/generated/**")
+        }
+
+        android.set(true)
+
         dependencies.add("ktlintRuleset", libs.findLibrary("ktlint.composeRules").get())
+
+        outputToConsole.set(true)
     }
 }
